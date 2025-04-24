ba30503c462e146ace5d7067c4ea2d276d58fae3
Alok Babu <124550479+Alokbabuyadhuvanshi@users.noreply.github.com>
2025-04-24 20:15:28 +0530

    Update settings.py

--- Telemedition/Telemedition/settings.py
+++ Telemedition/Telemedition/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'Telemedition.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Healio',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
